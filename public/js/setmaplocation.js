1f173b33d759f32da13519ecd48f630f0e69022f
Hiran Shah <hiran.shah4@gmail.com>
2023-05-01 22:42:22 +0100

    Change location with click event when adding new Furniture

--- public/js/setmaplocation.js
+++ public/js/setmaplocation.js
@@ -9,39 +9,44 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
-
 // Sets map data source and associates with map
 
 let marker, circle, zoomed;
-
 navigator.geolocation.watchPosition(success, error);
 
 function success(pos) {
   const lat = pos.coords.latitude;
   const lng = pos.coords.longitude;
   const accuracy = pos.coords.accuracy;
-
   if (marker) {
     map.removeLayer(marker);
     map.removeLayer(circle);
   }
   // Removes any existing marker and circule (new ones about to be set)
-
   marker = L.marker([lat, lng]).addTo(map);
   circle = L.circle([lat, lng], { radius: accuracy }).addTo(map);
   // Adds marker to the map and a circle for accuracy
-
   if (!zoomed) {
     zoomed = map.fitBounds(circle.getBounds());
   }
   // Set zoom to boundaries of accuracy circle
-
   map.setView([lat, lng]);
   // Set map focus to current user position
-
   document.getElementById("latitude").value = lat;
   document.getElementById("longitude").value = lng;
 }
+
+function onMapClick(e) {
+  if (marker) {
+    map.removeLayer(marker);
+    map.removeLayer(circle);
+  }
+  marker = L.marker(e.latlng);
+  marker.addTo(map);
+  document.getElementById("latitude").value = e.latlng.lat;
+  document.getElementById("longitude").value = e.latlng.lng;
+}
+map.on("click", onMapClick);
 
 function error(err) {
   if (err.code === 1) {
@@ -50,5 +55,3 @@ function error(err) {
     alert("Cannot get current location");
   }
 }
-
-// module.exports = { lat, lng };
